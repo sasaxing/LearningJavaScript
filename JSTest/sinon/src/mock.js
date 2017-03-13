013ffd9fadb10bac05798e21f1057dee6601dbdb
xiaoshax <xiaosha.xing@citrix.com>
2017-03-13 12:11:32 +0100

    JSTest/mocha-chai-sinon-cheatsheet/

--- JSTest/sinon/src/mock.js
+++ JSTest/sinon/src/mock.js
@@ -1,20 +1,27 @@
 const sinon = require('sinon')
 // mocha src/mock.js
 describe('Calculator', () => {
-    it('should do add', () => {
+    it('should do add and mul', () => {
         var aCalculator = {
             add: function (a, b) {
-                return 2
+                return a + b
+            },
+            mul: function (a, b) {
+                return a * b
             }
         }
         const mock = sinon.mock(aCalculator)
         mock.expects('add')
             .withArgs(1, 2)
             .atLeast(2)
+        mock.expects('mul')
+            .atMost(1)
 
         aCalculator.add(1, 2)
         aCalculator.add(1, 2)
 
         mock.verify()
+
+
     });
 });
