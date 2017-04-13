9323837e584ad8a3fb2c102d5c7985cf4f72228e
xiaoshax <xiaosha.xing@citrix.com>
2017-04-13 13:03:30 +0200

    koa-http.js

--- chpt20 Node/network/servers/koa/koa-http.js
+++ chpt20 Node/network/servers/koa/koa-http.js
@@ -19,6 +19,8 @@ app.use(async ctx => {
 })
 server.listen(3000);
 function closeServer() {
+    //it will not cause an error to throw if there is nothing to close.
+    // the cb function of server.close will be executed no mater what.
     server.close(function () {
         console.log('Server is closed.')
     })
