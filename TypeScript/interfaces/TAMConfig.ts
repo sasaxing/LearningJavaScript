882ab8f2f070b8fee42b9cfc5926cf3c0b723a33
Xiaosha61 <xxssfamily61@gmail.com>
2017-05-11 17:22:36 +0200

    TS/interfaces/TAMconfig.ts

--- TypeScript/interfaces/TAMConfig.ts
+++ TypeScript/interfaces/TAMConfig.ts
@@ -1,19 +1,37 @@
 export interface TAMConfig {
-    storagePolicies: Array<StorageConfig>,
+    storagePolicies: Array<StoragePolicyConfig>,
 }
 
-/**
- * Customs the requirements of TestArtifactManager handling the artifacts.
- */
-export interface StorageConfig {
-    //commen properties: e.g., duration...
+export interface StoragePolicyConfig {
+    provider: string
     port?: number // should we ever enable clients to provide port?
 }
 
-export interface LocalStorageConfig extends StorageConfig {
-    port: number // should we ever enable clients to provide port?
+export interface LocalStoragePolicyConfig extends StoragePolicyConfig {
+    artifactRoot?: string
 }
 
-export interface S3StorageConfig extends StorageConfig {
+export interface S3StoragePolicyConfig extends StoragePolicyConfig {
     bucketName?: string // which bucket to use
 }
+
+const localStorageConfig = <LocalStoragePolicyConfig>{
+    port: 3000,
+    provider: 'local'
+}
+
+const s3StorageConfig = <S3StoragePolicyConfig>{
+    port: 3000,
+    provider: 's3',
+    bucketName: 'aBucket'
+}
+
+
+const storagePolicies = Array<LocalStoragePolicyConfig | S3StoragePolicyConfig>()
+storagePolicies.push(localStorageConfig)
+storagePolicies.push(localStorageConfig)
+let s3SPCs = storagePolicies.filter((config) => config.provider === 's3')
+s3SPCs.forEach((config) => {
+    let s3spc = config as S3StoragePolicyConfig
+    console.log(s3spc.bucketName)
+})
