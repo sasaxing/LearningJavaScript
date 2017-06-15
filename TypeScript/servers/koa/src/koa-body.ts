10c72316e6489913c76df704e503ba00f9160a47
Xiaosha61 <xxssfamily61@gmail.com>
2017-06-15 11:27:47 +0200

    TS/koa/try http-status-codes

--- TypeScript/servers/koa/src/koa-body.ts
+++ TypeScript/servers/koa/src/koa-body.ts
@@ -1,6 +1,7 @@
 import * as Koa from 'koa'
 import * as body from 'koa-body'
 import * as path from 'path'
+import * as statusCode from 'http-status-codes'
 
 let app = new Koa();
 
@@ -15,7 +16,7 @@ app.use(body({
 app.use(async function (context) {
     //console.log(context.request.body.fields)
     //console.log(context.request.body.files)
-
+    context.response.status = statusCode.ACCEPTED //202
     context.set('error-message', '==> ERROR.....')
     context.response.body = context.response
 })
