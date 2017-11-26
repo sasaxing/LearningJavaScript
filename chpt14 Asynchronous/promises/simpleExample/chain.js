d01338bb4894fdc52923a243f5beb248cf1e3281
Xiaosha61 <xxssfamily61@gmail.com>
2017-11-26 19:51:58 +0100

    JS/14/promises/simpleExample/chain -- 2 experiments

--- chpt14 Asynchronous/promises/simpleExample/chain.js
+++ chpt14 Asynchronous/promises/simpleExample/chain.js
@@ -1,39 +1,85 @@
-let cleanRoom = function(){
-  return new Promise(function(resolve, reject) {
-    resolve('Cleaned the room.')
-  });
-};
-
-let removeGarbage = function(message){
-  return new Promise(function(resolve, reject) {
-    resolve(message + 'Remove garbage.')
-  });
-};
-
-let winIcecream = function(message){
-  return new Promise(function(resolve, reject) {
-    resolve(message + 'Win an ice cream.');
-  });
-};
-
-cleanRoom().then(function(result){ // when the obj returned by cleanRoom() decides to resolve, this function will be executed.
-  return removeGarbage(result);  //result = 'Clean the room.'
-}).then(function(result){  // when the obj returned by removeGarbage() decides to resolve, this function will be executed.
-  return winIcecream(result); // result = 'Clean the room. Remove garbage.'
-}).then(function(result){ // when the obj returned by winIcecream() decides to resolve, this function will be executed.
-  console.log('Finished: ' + result);      // result = 'Clean the room. Remove garbage. Win an ice cream.'
-});
-
-// this is how we can use promise to do the dependency
-// you don't need to nest it, you can define each function independently
+// You can use PromiseChain to deal with promises with the dependency
+// you don't need to nest them, you can define each function independently
 
 // you can also use nested functions to implement these processes
 // but when the callback becomes much more,
 // it can be a nightmire to do so much nested.
 
+let cleanRoom = function (input) {
+  return new Promise(function (resolve, reject) {
+    if (input === 'cleanRoomFail') {
+      reject('[cleanRoom] rejects.')
+    } else {
+      resolve(input + 'Cleaned the room;   ')
+    }
+  });
+};
 
+let removeGarbage = function (message) {
+  return new Promise(function (resolve, reject) {
+    if (message.indexOf('removeGarbageFail') !== -1) {
+      reject('[removeGarbage] rejects.')
+    }
+    resolve(message + 'Remove garbage;   ')
+  });
+};
 
-// these 3 functions have to be sequentially executed in this way
-// but the execution will not block the rest of the code
-// the following will be executed before.
-console.log("this is the end of the code.");
+let winIcecream = function (message) {
+  return new Promise(function (resolve, reject) {
+    if (message.indexOf('winIcecreamFail') !== -1) {
+      reject('[winIcecream] rejects.')
+    }
+    resolve(message + 'Win an ice cream.   ');
+  });
+};
+
+cleanRoom('winIcecreamFail')
+  .then(function (result) { // when the obj returned by cleanRoom() decides to resolve, this function will be executed.
+    return removeGarbage(result);  //result = 'Clean the room.'
+  })
+  .then(function (result) {
+    return winIcecream(result)
+  })
+  .then(function (result) {
+    console.log('finally resolve!!' + result)
+  })
+  .catch(function (reason) {
+    console.log(reason)
+  })
+
+// experiment1: promise.then(rs, rj) !== promise.then(rs).catch(rj), 
+// the latter uses promise chain, handling it when rs somehow also rejects.
+cleanRoom('cleanRoomnotFail')
+  .then((result) => {
+    return new Promise(function (resolve, reject) {
+      console.log(result)
+      reject('I am not happy') // this reject will be handled by the chained catch.
+    })
+  })
+  .catch((reason) => {
+    console.log(reason)
+  })
+/* - produce error.
+cleanRoom('cleanRoomnotFail')
+  .then((result) => {
+    return new Promise(function (resolve, reject) {
+      console.log(result)
+      reject('I am not happy') // this won't be handled -> UPRW.
+    })
+  }, (reason) => {
+    console.log(reason)
+  })
+*/
+
+// experiment2: 
+new Promise(function (resolve, reject) {
+  setTimeout(() => {
+    reject(); // can be handled by chained catch.
+  }, 10);
+}).catch(() => { });
+
+new Promise(function (resolve, reject) {
+  setTimeout(() => {
+    throw new Error("Whoops!"); // can not be handled by chained catch.
+  }, 10);
+}).catch(() => { });
