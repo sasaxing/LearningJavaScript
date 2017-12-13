cfbf1e4c72607a735506df42853e828d1ded9175
Xiaosha61 <xxssfamily61@gmail.com>
2017-12-13 19:43:58 +0100

    JS/cht10/iterateMap

--- chpt10 Map&Set/mapInitialization.js
+++
@@ -1,8 +0,0 @@
-const userRoles1 = new Map([
-    ['u1', 'User'],
-    ['u2', 'User'],
-    ['u3', 'Admin'],
-    ['u4', 'User'],
-]);
-console.log(`userRoles1.size = ${userRoles1.size}`); //3
-console.log(userRoles1)
