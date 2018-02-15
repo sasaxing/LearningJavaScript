2a9370a91e4aaa9d6dbcd607371ecf404220bd30
Xiaosha61 <xxssfamily61@gmail.com>
2018-02-15 13:00:02 +0100

    JS/Function/this

--- chpt6 Functions/this/this_in_obj.js
+++ chpt6 Functions/this/this_in_obj.js
@@ -1,12 +1,13 @@
 const xxs = {
   name : 'xiaosha',
-  sayhi: function() { return `hello, I am ${this.name}`; },
-
+  sayhi: function() { return `hello, I am ${this.name}`; }, 
+  //The 'this' here is refering to the context where this function is invoked by default.
 }
 
 const amy = {
   name : 'amy',
-  speak : xxs.sayhi,
+  speak : xxs.sayhi, // Thus, the 'this' in sayhi() will be bound to amy by default.
+  //speak : xxs.sayhi.bind(xxs), // unless you explicitely bind it to what you want.
 }
 
 console.log(`${amy.speak()}`)
