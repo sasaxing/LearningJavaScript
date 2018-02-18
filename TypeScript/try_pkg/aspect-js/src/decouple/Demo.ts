77a6c4f62c5ce0351b2de55daf8a7343b543de65
Xiaosha61 <xxssfamily61@gmail.com>
2018-02-18 12:12:33 +0100

    TS/try_pkg/aspect-js/decouple Dog and DogAspect -- use 'require' instead of 'import'

--- TypeScript/try_pkg/aspect-js/src/decouple/Demo.ts
+++ TypeScript/try_pkg/aspect-js/src/decouple/Demo.ts
@@ -1,5 +1,11 @@
 import { Dog } from './Dog'
-import { DogAspect } from './DogAspect'
-export class MyAspect extends DogAspect { } // connects Dog and DogAspect in the same file.
+import { Cat } from './Cat'
+require('./Aspects') // this will make Aspect run at compile time -- help to finish the meave process.
+//import {DogAspect} from './Aspect' does not work because this does not trigger Aspect to be executed.
+//but in order to insert advice to Dog, we must get DogAspect run in compile time.
+
 const myDog = new Dog()
 myDog.bark()
+
+const myCat = new Cat()
+myCat.meow()
