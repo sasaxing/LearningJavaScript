6b5aa5a44d7837f57d326edaacd23f19aece9d7c
Xiaosha61 <xxssfamily61@gmail.com>
2018-05-02 23:15:42 +0200

    lerna

--- chpt1 Application/main.js
+++ chpt1 Application/main.js
@@ -1,23 +1,28 @@
-$(document).ready(function(){
+$(document).ready(function () {
   'use strict';
   paper.install(window);
   paper.setup(document.getElementById('mainCanvas'));
 
   // TODO
-  var c = Shape.Circle(200,200, 80);
+  var c = Shape.Circle(200, 200, 80);
   c.fillColor = 'black';
-  var text = new PointText(200,200);
-  text.justification= 'center';
+  var text = new PointText(200, 200);
+  text.justification = 'center';
   text.fillColor = 'white';
   text.fontSize = 20;
   text.content = 'hello world';
 
   var tool = new Tool();
-  tool.onMouseDown= function(event){
-    var c=Shape.Circle(event.point, 20);
-    c.fillColor= 'green';
+  tool.onMouseDown = function (event) {
+    var c = Shape.Circle(event.point, 20);
+    c.fillColor = 'green';
   };
 
   paper.view.draw();
   console.log('main.js loaded');
+  console.log('main.js....');
+  console.log(document.getElementById('private_msg').innerText)
+  console.log(document.getElementsByTagName('div')[0].innerText)
+  console.log(document.cookie)
+  console.log(document)
 })
