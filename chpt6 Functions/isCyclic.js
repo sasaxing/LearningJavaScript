b32e40fb830fe6d843da7a86d970355c2ef84746
Xiaosha61 <xing.xiaosha61@gmail.com>
2018-05-28 14:22:10 +0200

    6/isCyclic

--- chpt6 Functions/isCyclic.js
+++ chpt6 Functions/isCyclic.js
@@ -1,4 +1,3 @@
-
 function isCyclic(obj) {
     var seenObjects = [];
 
@@ -21,14 +20,43 @@ function isCyclic(obj) {
     return detect(obj);
 }
 
-a = {}
-b = {}
-a.b = b; b.a = a;
-console.log(isCyclic(a));
+function isTooDeep(object, callCount=0){
+    if(callCount > 11) {
+        return true;
+    }
+    for(let key of Object.keys(object)){
+        if(object[key] === object) { return true; } //once detect cyclic, return true.
+        if(typeof(object[key]) === 'object'){
+            if(isTooDeep(object[key], ++callCount)){
+                return true; // otherwise continue checking the other properties.
+            }
+        }
+    }
+    return false;
+}
+
+o1 = { a: 1 };
+console.log('\n==> o1:');
+console.log(isCyclic(o1));
+console.log(isTooDeep(o1));
 
 c1 = { a: 1 };
 c1.c = c1;
+console.log('\n==> c1:');
 console.log(isCyclic(c1));
+console.log(isTooDeep(c1));
 
-o1 = { a: 1 };
-console.log(isCyclic(o1));
+a = {}
+b = {}
+a.b = b; b.a = a;
+console.log('\n==> a:');
+console.log(isCyclic(a));
+console.log(isTooDeep(a));
+
+const obj1 = {b1: {}};
+const obj2 = {a2: obj1};
+obj1.c = obj2;
+
+console.log('\n==> obj1:');
+console.log(isCyclic(obj1))
+console.log(isTooDeep(obj1));
